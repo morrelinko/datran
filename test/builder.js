33d96aa329156d028f46464b0c1e74c79b2af164
Laju Morrison <morrelinko@gmail.com>
2018-01-06 05:50:12 +0100

    use parser abstraction in builder + some builder tests added

--- test/builder.js
+++ test/builder.js
@@ -1,3 +1,34 @@
 'use strict'
 
-describe('builder.toObject()', function () {})
+const { assert } = require('chai')
+const types = require('../lib/types')
+const Builder = require('../lib/builder')
+
+describe('builder', function () {
+  it('transforms item resource using callback', async function () {
+    let resource = new types.Item({ foo: 'bar' }, function (model) {
+      return { foo_key: model.foo }
+    })
+
+    let builder = new Builder(resource)
+    let data = await builder.toObject()
+    let json = await builder.toJson()
+
+    assert.hasAllKeys(data, ['foo_key'])
+    assert.equal(json, '{"foo_key":"bar"}')
+  })
+
+  it('transforms collection resource using callback', async function () {
+    let resource = new types.Collection(
+      [{ foo: 'bar' }, { some: 'thing' }],
+      function (model) {
+        return model
+      }
+    )
+
+    let builder = new Builder(resource)
+    let json = await builder.toJson()
+
+    assert.equal(json, '[{"foo":"bar"},{"some":"thing"}]')
+  })
+})
